e3e03505ec28f5d47292796b9459d54da85250b7
nickysll <nickysll@hotmail.com>
2025-05-05 20:55:05 -0300

    Contador de Letras

---
+++ Contador.py
@@ -0,0 +1,21 @@
+print("Digite uma palavra para que o programa conte as letras")
+palavra = str(input("Digite uma palavra: ")).upper()
+
+contador = len(palavra)
+print(f"A palavra {palavra} tem {contador} letras.")
+
+vogal = ["A", "E", "I", "O", "U"]
+
+consoantes = 0
+vogais = 0
+
+for i in palavra:
+    if i not in vogal:
+        consoantes += 1
+    else:
+        vogais += 1
+print(
+    f"A Palavra tem {contador} letras, {consoantes} consoantes e {vogais} vogais")
+
+'''print("A palavra tem:", palavra.count("A") + palavra.count("E") +
+      palavra.count("I") + palavra.count("O") + palavra.count("U"), "vogais")'''
